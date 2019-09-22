b466a855b5f9813172a750cf69192c0f1fd91413
Marcos <mp2772@gmail.com>
2019-09-22 00:06:38 -0400

    updated public folder with styling from front-end

---
+++ public/precache-manifest.cacd3493e0dd92ebd1e17e55f806fcc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c81db6f916461cb2e9a8a64add456ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1c7b6a7ea974f22c3c",
+    "url": "/static/css/main.2e95d1ae.chunk.css"
+  },
+  {
+    "revision": "86adcf196a263390d317",
+    "url": "/static/js/2.d4ab1421.chunk.js"
+  },
+  {
+    "revision": "9d1c7b6a7ea974f22c3c",
+    "url": "/static/js/main.9455fb61.chunk.js"
+  },
+  {
+    "revision": "f97737bfeab2e4cb779c",
+    "url": "/static/js/runtime-main.cd483476.js"
+  }
+]);
